0960c3c88e724d3616e6ab6590137d17e336fb9e
MohammedAzim Shaikh <azim.music9@gmail.com>
2022-05-20 18:55:04 +0530

    Updated on 2022-05-20 18:55:04

--- picnic_app.py
+++ picnic_app.py
@@ -87,12 +87,13 @@ with form:
     cols = st.columns((1, 1))
     author = cols[0].text_input("Name:")
     bug_type = cols[1].selectbox(
-        "Address:", ["Front-end", "Back-end", "Data related", "404"], index=2
+        "Address:", ["Tawakkal Villa", "Back-end", "Data related", "404"], index=2
     )
-    comment = st.text_area("Comment:")
+   
     cols = st.columns(2)
     date = cols[0].date_input("Bug date occurrence:")
     bug_severity = cols[1].slider("Bug severity:", 1, 5, 2)
+    comment = st.text_area("Comment:")
     submitted = st.form_submit_button(label="Submit")
 
 
